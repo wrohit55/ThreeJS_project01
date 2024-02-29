d5ebd73345c7b84a79c756fe967e4bfea8a256c5
wrohit55 <rohitdahal75@gmail.com>
2024-02-29 21:33:56 +0545

    navbar started

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
-import { About, Contact, Experince, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
 
 const App = () => {
- 
+
 
   return (
-    <>
-     <div>
     
-     </div>
-    </>
+    <BrowserRouter>
+        <div className="relative z-0 bg-black">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+        </div>
+      </BrowserRouter>
   )
 }
 
