01e2554f2cd7932ab273835704a0484ad96cf25d
wrohit55 <rohitdahal75@gmail.com>
2024-03-01 21:28:02 +0545

    contact section added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact">
+      <div class="contact-form-container">
+        <h2>Contact Us</h2>
+        <form id="contact-form">
+          <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+          </div>
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+          </div>
+          <div class="form-group">
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+          </div>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
