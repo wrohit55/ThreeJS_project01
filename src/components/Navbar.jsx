e5f010e7f8898254d80ea3a4e66ac3949863ce99
wrohit55 <rohitdahal75@gmail.com>
2024-03-02 23:16:28 +0545

    Naver section is being developed but catch somne errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { styles } from './styles'
 import { logo, menu, close } from '../assets';
+import { navLinks } from './constants';
 
 
 const Navbar = () => {
@@ -11,8 +12,32 @@ const Navbar = () => {
   return (
     <nav className={'${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary'}
     >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto"></div>
-      hello
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/"
+        className="flex items-center gap-2"
+        onClick={()=>{
+          setActive("");
+          window.scrollTo(0,0);
+        }}>
+          <img src={logo} alt='Logo' className='w-12 h-12 object-contain' />
+          <p className='text-whit text-[18px] font-bold cursor-pointer'>Youth IT <span className='sm:block hidden'>| Coding the future of tech </span></p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link)=> (
+            <li
+            // key={link.id}
+            // className={`${
+            //   active === link.title
+            //   ? "text-white"
+            //   : "text-secondary"
+            // } hover:text-white text-[18px] font-medium cursor-pointer`}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      
 
     </nav>
   )
