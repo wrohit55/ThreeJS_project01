0b468222b94c73608920bbccbdda873ea23b153b
wrohit55 <rohitdahal75@gmail.com>
2024-03-08 21:14:23 +0545

    ball section started

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Ball = () => {
   return (
-    <div>Ball</div>
+    <body class="flex items-center justify-center h-screen bg-gray-200">
+    
+    <div class="w-24 h-24 bg-blue-500 rounded-full"></div>
+</body>
   )
 }
 
