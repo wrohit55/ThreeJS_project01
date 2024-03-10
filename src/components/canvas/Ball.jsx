0fd2b45c7c5171015aa73ee1fb3cc1d4e5b097c6
wrohit55 <rohitdahal75@gmail.com>
2024-03-10 22:30:54 +0545

    ball section removed

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 
 const Ball = () => {
   return (
-    <body class="flex items-center justify-center h-screen bg-gray-200">
-    
-    <div class="w-24 h-24 bg-blue-500 rounded-full"></div>
-</body>
+  <div className="ball"><h1>hello</h1></div>
   )
 }
 
